04cb8bce831b9271871ba14c0f6fb45319f89f22
Patricia <patysiq@yahoo.com.br>
2021-05-10 20:43:22 -0300

    Initial Commit

---
+++ AnimatedCircleLiquid/AnimatedCircleLiquidApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedCircleLiquidApp.swift
+//  AnimatedCircleLiquid
+//
+//  Created by PATRICIA S SIQUEIRA on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedCircleLiquidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
